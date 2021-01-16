91496ffef8f99cae8e7696c146672a266d14c005
asantony <tonyylg@gmail.com>
2021-01-16 08:18:22 +0530

    Updates

---
+++ precache-manifest.85f903dc61c73c1f75df8b7baf521de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18dec8bb9d4a440a76a7",
+    "url": "/asantony/react-bootstrap/static/css/main.028e87f5.chunk.css"
+  },
+  {
+    "revision": "18dec8bb9d4a440a76a7",
+    "url": "/asantony/react-bootstrap/static/js/main.18dec8bb.chunk.js"
+  },
+  {
+    "revision": "1048ea6de5a7d7dd4d0f",
+    "url": "/asantony/react-bootstrap/static/js/1.1048ea6d.chunk.js"
+  },
+  {
+    "revision": "6bcef3837537e7c95c2c",
+    "url": "/asantony/react-bootstrap/static/js/runtime~main.6bcef383.js"
+  },
+  {
+    "revision": "1e3ae2e567053bdc642361a707bf2173",
+    "url": "/asantony/react-bootstrap/index.html"
+  }
+];
